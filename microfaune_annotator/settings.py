db35f03a7f643e866ce72d4004275477f1a428b1
hadrienj <hadrienjean@gmail.com>
2019-11-28 21:28:34 +0100

    Update static config

--- microfaune_annotator/settings.py
+++ microfaune_annotator/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 
 MEDIA_URL = '/media/'
