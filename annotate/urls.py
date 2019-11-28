b77766e86373ac68af933f6fcd69f33d97f9e888
hadrienj <hadrienjean@gmail.com>
2019-11-28 22:56:11 +0100

    Uses whitenoise"

--- annotate/urls.py
+++ annotate/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.homepage, name="home"),
