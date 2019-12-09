8e52f1df1b55ff4572e33c702cc0688d9a1abac4
florent <florent@vitadx.com>
2019-12-09 15:47:05 +0100

    Add example upload script

---
+++ upload_scripts/upload_annotations.py
@@ -0,0 +1,30 @@
+from glob import glob
+import json
+import os
+
+
+if __name__ == "__main__":
+    json_dump_file = "dump_for_upload.json" 
+    audio_ext = ".wav"
+    username = "annotator"
+    annotation_dir = "../../data/labelled_json/"
+
+    annotation_files = sorted(glob(os.path.join(annotation_dir, "*_labeled.json")))
+
+    d = [] 
+    for a_f in annotation_files: 
+         
+        json_filename = os.path.basename(a_f) 
+        wav_name = json_filename[:-13] + audio_ext
+        print(wav_name) 
+        with open(a_f, "r") as f: 
+            s = f.read() 
+        if s: 
+            tmp_data = json.loads(s) 
+        else: 
+            tmp_data = [] 
+        d.append({"file": wav_name, "username": username, "value": tmp_data})
+
+    with open(json_dump_file, "w") as f:
+        json.dump(d, f, indent=2)
+
