290481cbc03b2b19dd8adbe240a4e26018b89527
florent <florent@vitadx.com>
2019-12-10 13:22:19 +0100

    Update upload scripts with bacth

--- upload_scripts/dump_predictions.py
+++ upload_scripts/dump_predictions.py
@@ -8,19 +8,25 @@ from microfaune.detection import RNNDetector
 
 if __name__ == "__main__":
     track_dir = "../../data/citeU/"
-    json_dump_file = "predictions.json"    
+    json_dump_file = "predictions_{ind}.json"    
 
     tracks = sorted(glob(os.path.join(track_dir, "*.wav")))
     
     model = RNNDetector()
 
     data = []
+    ind = 0
     for audiofile in tracks:
         _, local_score = model.predict_on_wav(audiofile)
         ogg_file = os.path.basename(audiofile).replace("wav", "ogg")
         data.append({"file": ogg_file,
                      "prediction": [float(s) for s in local_score]})
+        if len(data) == 100:
+            with open(json_dump_file.format(ind=ind), "w") as f:
+                json.dump(data, f)
+            data = []
+            ind += 1
 
-    with open(json_dump_file, "w") as f:
+    with open(json_dump_file.format(ind=ind), "w") as f:
         json.dump(data, f)
 
