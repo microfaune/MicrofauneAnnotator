909244a7f93aea26951f4ccf31d3cd3f9c1d6d9c
florent <florent@vitadx.com>
2019-12-10 13:33:12 +0100

    Fix for pep8

---
+++ upload_scripts/dump_annotations.py
@@ -0,0 +1,29 @@
+from glob import glob
+import json
+import os
+
+
+if __name__ == "__main__":
+    json_dump_file = "annotations.json"
+    audio_ext = ".ogg"
+    username = "annotator"
+    annotation_dir = "../../data/labelled_json/"
+
+    annotation_files = sorted(glob(os.path.join(annotation_dir,
+                                                "*_labeled.json")))
+
+    d = []
+    for a_f in annotation_files:
+        json_filename = os.path.basename(a_f)
+        wav_name = json_filename[:-13] + audio_ext
+        print(wav_name)
+        with open(a_f, "r") as f:
+            s = f.read()
+        if s:
+            tmp_data = json.loads(s)
+        else:
+            tmp_data = []
+        d.append({"file": wav_name, "username": username, "value": tmp_data})
+
+    with open(json_dump_file, "w") as f:
+        json.dump(d, f, indent=2)
